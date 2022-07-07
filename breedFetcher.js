1702bd9d74a390e60437a3ceb8be20efd3dc0c4e
Wesley Corner <wescorner@gmail.com>
2022-07-07 22:11:27 +0000

    tabs to spaces

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,15 @@ const request = require("request");
 const breed = process.argv[2];
 
 request(
-	`http://api.thecatapi.com/v1/breeeds/search?q=${breed}`,
-	(error, response, body) => {
-		if (error) console.log(error);
-		else if (body === "[]") console.log("No breed found!");
-		else if ("message" in JSON.parse(body))
-			console.log(JSON.parse(body).message);
-		else {
-			const data = JSON.parse(body);
-			console.log(data[0].description);
-		}
-	}
+  `http://api.thecatapi.com/v1/breeeds/search?q=${breed}`,
+  (error, response, body) => {
+    if (error) console.log(error);
+    else if (body === "[]") console.log("No breed found!");
+    else if ("message" in JSON.parse(body))
+      console.log(JSON.parse(body).message);
+    else {
+      const data = JSON.parse(body);
+      console.log(data[0].description);
+    }
+  }
 );
